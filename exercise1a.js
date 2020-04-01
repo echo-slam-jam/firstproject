adad15ab1a64c43134a4459b8fce5a57dfd3ff78
Your Name <you@example.com>
2020-04-01 17:05:17 +0800

    changing case to if-else

--- exercise1a.js
+++ exercise1a.js
@@ -1,16 +1,11 @@
 for (var num = 1; num < 101; num++) {
- switch (true) {
-   case num % 3 == 0 && num % 5 == 0:
-     console.log("INTEL");
-     break;
-   case num % 3 == 0:
-     console.log("IN");
-     break;
-   case num % 5 == 0:
-     console.log("TEL");
-     break;
-   default:
-     console.log(num);
-     break;
+ if (num % 3 === 0 && num % 5 === 0) {
+   console.log("INTEL");
+ } else if (num % 3 === 0) {
+   console.log("IN");
+ } else if (num % 5 === 0){
+   console.log("TEL");
+ }else{
+   console.log(num);
  }
-}
+ }
